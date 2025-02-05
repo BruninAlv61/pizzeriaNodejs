d8712174d042a6ed53b57072cbc9493331b44706
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-02-05 12:32:11 -0300

    borrando test.js

--- server/test.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-console.log('MONGO_ATLAS_URI:', process.env.MONGO_ATLAS_URI) // Verifica la URI
-
-const uri = process.env.MONGO_ATLAS_URI
-if (!uri) throw new Error('❌ ERROR: MONGO_ATLAS_URI no está definido en .env')
