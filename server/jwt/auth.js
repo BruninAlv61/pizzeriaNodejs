b060609a024190967f1350eafc742444e45076e6
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-02-19 18:34:05 -0300

    preparando admin-panel y jwt para los cambios de la nueva rama

--- server/jwt/auth.js
+++ server/jwt/auth.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken'
 import { SECRET_JWT_KEY } from '../config.js'
 
 export const setAuthCookie = (res, payload) => {
-  // Generar accessToken
+  // Generar accessToken con 15 minutos de expiración
   const accessToken = jwt.sign(payload, SECRET_JWT_KEY, {
-    expiresIn: '1m'
+    expiresIn: '15m'
   })
-  // Establecer cookies
+  // Establecer cookies con 15 minutos de expiración
   res.cookie('access_token', accessToken, {
     httpOnly: true,
     sameSite: 'strict',
-    maxAge: 1000 * 60 * 1
+    maxAge: 1000 * 60 * 15 // 15 minutos en milisegundos
   })
 }
