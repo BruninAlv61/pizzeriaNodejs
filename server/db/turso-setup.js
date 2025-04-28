dbd9bf27fdd33bd54cfd58ab46692e7a68a68ad9
BruninAlv61 <alvarezbruno.dev@gmail.com>
2025-04-28 20:41:21 -0300

    add new tables

--- server/db/turso-setup.js
+++ server/db/turso-setup.js
@@ -60,3 +60,57 @@ await db.execute(`
     password    TEXT NOT NULL 
   )
 `)
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS customers (
+    customer_id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone_number TEXT
+  )
+`)
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS orders (
+    order_id TEXT PRIMARY KEY,
+    customer_id TEXT NOT NULL,
+    branch_id TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    order_status TEXT NOT NULL,
+    created_at TEXT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (branch_id) REFERENCES branches(branch_id)
+  )
+`)
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS physical_sales (
+    physical_sales_id TEXT PRIMARY KEY,
+    customer_id TEXT,
+    sale_date TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    payment_method TEXT NOT NULL,
+    customer_phone TEXT,
+    branch_id TEXT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (branch_id) REFERENCES branches(branch_id)
+  )
+`)
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS sales_items (
+    sales_item_id TEXT PRIMARY KEY,
+    physical_sale_id TEXT,
+    order_id TEXT,
+    product_id TEXT,
+    combo_offers_id TEXT,
+    quantity INTEGER NOT NULL,
+    price REAL NOT NULL,
+    FOREIGN KEY (physical_sale_id) REFERENCES physical_sales(physical_sales_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES menu(product_id),
+    FOREIGN KEY (combo_offers_id) REFERENCES combo_offers(combo_offers_id)
+  )
+`)
